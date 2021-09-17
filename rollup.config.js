56abf0ef4fc0d53af4ad1b1bd0788213f4617f96
Albert Moravec <albert.moravec@gmail.com>
2021-09-18 00:16:01 +0200

    Use Caddyfile

--- rollup.config.js
+++ rollup.config.js
@@ -93,7 +93,7 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser(),
+		// production && terser(),
 
 		html({
 			template: template,
