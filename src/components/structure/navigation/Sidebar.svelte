e7f33cf351e2dcd7932ebddf9d95e95d3eb05b42
Albert Moravec <albert.moravec@gmail.com>
2021-09-19 01:00:15 +0200

    Fix nav links

--- src/components/structure/navigation/Sidebar.svelte
+++ src/components/structure/navigation/Sidebar.svelte
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
+  <a href="#/" class="brand-link">
     <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8"> -->
     <span class="brand-text font-weight-light">AdminLTE 3</span>
--- src/components/structure/navigation/TopNavigation.svelte
+++ src/components/structure/navigation/TopNavigation.svelte
@@ -16,7 +16,7 @@
     <MobileNavToggle />
 
     <li class="nav-item d-none d-sm-inline-block">
-      <a href="index3.html" class="nav-link">Home</a>
+      <a href="#/" class="nav-link">Home</a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
       <a href="#" class="nav-link">Contact</a>
