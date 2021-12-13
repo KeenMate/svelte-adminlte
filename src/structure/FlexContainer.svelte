d49480f3f6cdb813f0d447a1113797e55c413d6e
ZelvaMan <zelvaman16@gmail.com>
2021-12-13 19:13:38 +0100

    revorked FlexContainer

--- src/structure/FlexContainer.svelte
+++ src/structure/FlexContainer.svelte
@@ -1,16 +1,21 @@
 <script>
 	export let row = false;
+	export let inline = false;
+	export let collumn = false;
+	export let justifyContent = null;
+	export let alignItems = null;
+
 </script>
 
-<div class:row={row} class="flex-container {$$props.class || ''}">
+<div class:row={row} class="{inline ? "d-inline-flex" : "d-flex"} {justifyContent ? "justify-content-" + justifyContent : ""} {alignItems ? "align-items-" + alignItems : ""}{$$props.class || ''}" class:flex-column={collumn}>
 	<slot />
 </div>
 
-<style>
+<!-- <style>
 .flex-container{
 	display:flex;
 	flex-wrap: wrap;
 	gap: 0.3em;
 	
 }
-</style>
+</style> -->
