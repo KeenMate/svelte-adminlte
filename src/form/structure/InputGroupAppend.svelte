97246cfd195b1094f1be8f20782c014beaeda0b2
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2021-12-17 19:24:01 +0100

    updated right branch this time

--- src/form/structure/InputGroupAppend.svelte
+++ src/form/structure/InputGroupAppend.svelte
@@ -1,3 +1,14 @@
+<script>
+  export let text = false;
+</script>
+
 <span class="input-group-append {$$props.class || ''}">
+  {#if !text}
   <slot/>
+  {:else}
+  <div class="input-group-text"> 
+    <slot/>
+  </div>
+  {/if}
+  
 </span>
