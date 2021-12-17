41d32e8b677aa4566ee0f7e876a7d5ea1ab0624d
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2021-12-17 21:18:23 +0100

    fixing uploadModal

--- src/form/UploadModal.svelte
+++ src/form/UploadModal.svelte
@@ -7,7 +7,7 @@
 	import XHRUpload from "@uppy/xhr-upload"
 	import "../../../node_modules/@uppy/core/dist/style.css"
 	import "../../../node_modules/@uppy/dashboard/dist/style.css"
-	import toastr from "../../helpers/toastr-helpers"
+	import toastr from "../toasts/toastr"
 
 	const dispatch = createEventDispatcher()
 
