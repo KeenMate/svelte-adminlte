f4af79b607d5ff28b80d3624f8ea55ea832038b3
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2021-12-17 19:15:48 +0100

    added text to InputGroup

--- src/components/form/structure/InputGroupPrepend.svelte
+++ src/components/form/structure/InputGroupPrepend.svelte
@@ -1,3 +1,15 @@
+
+<script>
+  export let text = false;
+</script>
+
 <span class="input-group-prepend {$$props.class || ''}">
+  {#if !text}
   <slot/>
+  {:else}
+  <div class="input-group-text"> 
+    <slot/>
+  </div>
+  {/if}
+  
 </span>
