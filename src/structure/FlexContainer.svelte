7f62e1b7352f3e4434ee1c4bfd3f8bd34c926cd9
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2021-12-21 20:12:18 +0100

    added wrap to FlexContainer

--- src/structure/FlexContainer.svelte
+++ src/structure/FlexContainer.svelte
@@ -4,10 +4,11 @@
 	export let collumn = false;
 	export let justifyContent = null;
 	export let alignItems = null;
+	export let wrap = true;
 
 </script>
 
-<div class:row={row} class="{inline ? "d-inline-flex" : "d-flex"} {justifyContent ? "justify-content-" + justifyContent : ""} {alignItems ? "align-items-" + alignItems : ""}{$$props.class || ''}" class:flex-column={collumn}>
+<div class:row={row} class="{inline ? "d-inline-flex" : "d-flex"} {justifyContent ? "justify-content-" + justifyContent : ""} {alignItems ? "align-items-" + alignItems : ""} {wrap ? "flex-wrap": ""}{$$props.class || ''}" class:flex-column={collumn}>
 	<slot />
 </div>
 
