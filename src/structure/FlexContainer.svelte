b0c0dc41e3dde274423d835da07c72d74da20f6f
Filip Jakab <jakabfilip.fj@gmail.com>
2022-01-04 00:21:32 +0100

    Added missing components and made package compilable

---
+++ src/structure/FlexContainer.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let row = false;
+	export let inline = false;
+	export let collumn = false;
+	export let justifyContent = null;
+	export let alignItems = null;
+	export let wrap = true;
+
+</script>
+
+<div class:row={row} class="{inline ? "d-inline-flex" : "d-flex"} {justifyContent ? "justify-content-" + justifyContent : ""} {alignItems ? "align-items-" + alignItems : ""} {wrap ? "flex-wrap": ""} {$$props.class || ''}" class:flex-column={collumn}>
+	<slot />
+</div>
+
+<!-- <style>
+.flex-container{
+	display:flex;
+	flex-wrap: wrap;
+	gap: 0.3em;
+	
+}
+</style> -->
