e0718cd76c5d5b22867039feec80390a045bafb9
Filip Jakab <jakabfilip.fj@gmail.com>
2022-01-05 01:33:05 +0100

    Added slot for sidebar header

--- src/structure/navigation/Sidebar.svelte
+++ src/structure/navigation/Sidebar.svelte
@@ -12,7 +12,9 @@
   <a href="#/" class="brand-link">
     <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8"> -->
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <span class="brand-text font-weight-light">
+      <slot name=header></slot>
+    </span>
   </a>
 
   <!-- Sidebar -->
