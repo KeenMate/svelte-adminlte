b9241ad9f29fd5326bc9f4cf9db11102c1810c5f
Filip Jakab <jakabfilip.fj@gmail.com>
2022-01-11 17:37:03 +0100

    Added phone input

---
+++ src/form/input/PhoneInput.svelte
@@ -0,0 +1,40 @@
+<script>
+  import Inputmask from "inputmask";
+
+  export let id = "";
+  export let value = "";
+  export let name = "";
+  export let placeholder = "";
+  export let pattern = null;
+  export let readonly = false;
+  export let disabled = false;
+  export let inputMask = null;
+  export let invalid = false;
+  export let maxlength = ""
+  export let size = "sm"
+
+  export let inputElement = null;
+
+  export function isValid() {
+    return inputElement.validity.valid;
+  }
+
+  $: inputElement && Inputmask().mask(inputElement);
+</script>
+
+<input
+  bind:this={inputElement}
+  bind:value
+  class="form-control form-control-{size} {$$props.class || ''}"
+  class:is-invalid={invalid}
+  type="tel"
+  data-inputmask={inputMask || ""}
+  {id}
+  {name}
+  {placeholder}
+  {pattern}
+  {readonly}
+  {disabled}
+  {maxlength}
+  on:keypress
+/>
