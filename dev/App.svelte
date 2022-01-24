9928314d104b67bae23589c802ed608885e2dda5
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-01-24 16:57:34 +0100

    added dev directory for testing components

---
+++ dev/App.svelte
@@ -0,0 +1,7 @@
+
+<script>
+	import Page from "./Page.svelte"
+</script>
+
+<Page/>
+
---
+++ dev/Page.svelte
@@ -0,0 +1,12 @@
+<h1 class="test-class">
+  This is the place for testing components
+
+</h1>
+
+<style>
+  .test-class{
+    color: red;
+    font-weight: bolder;
+  }
+
+</style>
