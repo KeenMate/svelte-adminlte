85892a7dc4d95b40ffad0f59d5bd1de0612e1892
Filip Jakab <jakabfilip.fj@gmail.com>
2022-04-06 02:34:56 +0200

    Removed id from DropdownButton.svelte and fixed href that interferes with spa hash location

--- src/structure/navigation/dropdown/DropdownButton.svelte
+++ src/structure/navigation/dropdown/DropdownButton.svelte
@@ -1,7 +1,7 @@
 <script>
-  export let href = "#";
+  export let href = "javascript:void(0)"
 </script>
 
-<a {href} on:click class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" {...$$restProps}>
-  <slot />
+<a {href} on:click class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" {...$$restProps}>
+	<slot />
 </a>
