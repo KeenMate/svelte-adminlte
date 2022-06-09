88d85bdb1b1ae95cb536005d5234c87d644f9b61
Filip Jakab <jakabfilip.fj@gmail.com>
2022-06-09 02:48:12 +0200

    Moved loader parent into Loader.svelte component so it is unified once and for all

--- src/ui/Loader.svelte
+++ src/ui/Loader.svelte
@@ -1,26 +1,40 @@
-<div class="lds-dual-ring center-block"></div>
+<div class="loader-parent" {...$$props}>
+	<div>
+		<div class="lds-dual-ring center-block"></div>
+	</div>
+</div>
 
 <style lang="sass">
-	.center-block
-		display: block !important
-
-
-	.lds-dual-ring
-		display: inline-block
-		width: 100%
+	.loader-parent
+		display: flex
+		justify-content: center
+		align-items: center
 		height: 100%
+		//min-height: 60vh
 
-		&::after
-			content: " "
-			display: block
-			width: 75%
-			height: 75%
-			margin: 4px
-			border-radius: 50%
-			border: 2px solid
-			border-color: rgb(255, 204, 0) transparent rgb(221, 45, 14) transparent
-			animation: lds-dual-ring 0.8s ease-in-out infinite
+		& > div
+			width: 4rem
+			height: 4rem
 
+			.center-block
+				display: block !important
+
+
+			.lds-dual-ring
+				display: inline-block
+				width: 100%
+				height: 100%
+
+				&::after
+					content: " "
+					display: block
+					width: 75%
+					height: 75%
+					margin: 4px
+					border-radius: 50%
+					border: 2px solid
+					border-color: rgb(255, 204, 0) transparent rgb(221, 45, 14) transparent
+					animation: lds-dual-ring 0.8s ease-in-out infinite
 
 	@keyframes lds-dual-ring
 		0%
