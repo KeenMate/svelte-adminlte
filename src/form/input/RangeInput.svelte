54c4029be43354bc79602c7b1ad863569279c827
Filip Jakab <jakabfilip.fj@gmail.com>
2022-08-05 01:18:15 +0200

    Added range input

---
+++ src/form/input/RangeInput.svelte
@@ -0,0 +1,19 @@
+﻿<script>
+	export let id
+	export let value
+	export let min
+	export let max
+</script>
+
+<input
+	bind:value
+	type="range"
+	class="custom-range {$$props.class || ''}"
+	{id}
+	{min}
+	{max}
+	on:input
+	on:change
+	on:focus
+	on:blur
+>
