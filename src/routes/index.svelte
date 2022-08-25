d82cc63e8af67e34f2a662e5de571da44ca29fce
Filip Jakab <jakabfilip.fj@gmail.com>
2022-08-25 19:07:42 +0200

    Lowered AdminLTE version to remove issue with undefined variable $colors-alt

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '@fortawesome/fontawesome-free/css/all.css';
 	import 'icheck-bootstrap/icheck-bootstrap.css';
-	import 'admin-lte/build/scss/adminlte.scss';
+	import 'admin-lte/build/scss/AdminLTE.scss';
 	import 'toastr/build/toastr.css';
 	// import 'admin-lte/dist/js/adminlte';
 
--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import sveltePreprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: true
+		})
 	},
 	preprocess: sveltePreprocess()
 };
