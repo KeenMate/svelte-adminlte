90d544a36f81ac5adffb8a382d44c9af0e811d21
Filip Jakab <jakabfilip.fj@gmail.com>
2022-09-07 00:28:30 +0200

    Fixed bug with prefixed props

--- src/lib/helpers/object-helpers.js
+++ src/lib/helpers/object-helpers.js
@@ -1,5 +1,14 @@
+/**
+ * @param object {object} expects keys to be lowerCamelCase
+ * @param prefix {string} prefix to search by
+ * @returns {object} new object with filtered keys
+ */
 export function getKeysByPrefix(object, prefix) {
 	return Object.keys(object)
 		.filter(x => x.startsWith(prefix))
-		.reduce((acc, x) => (acc[x.substring(prefix.length)] = object[x], acc))
+		.reduce((acc, x) => (acc[extractPrefix(x, prefix)] = object[x], acc))
+}
+
+function extractPrefix(field, prefix) {
+	return field.substring(prefix.length).replace(/^\w/, x => x.toLocaleLowerCase())
 }
