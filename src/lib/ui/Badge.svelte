950735a56560e25a3c213ee14fc640f41a1ec2ba
Filip Jakab <jakabfilip.fj@gmail.com>
2022-11-03 00:40:08 +0100

    Added missing emit of click event for Badge.svelte

--- src/lib/ui/Badge.svelte
+++ src/lib/ui/Badge.svelte
@@ -2,6 +2,6 @@
 	export let color = ""
 </script>
 
-<span class="badge {(color && 'bg-' + color) || ''} {$$props.classes || ''}">
+<span class="badge {(color && 'bg-' + color) || ''} {$$props.classes || ''}" on:click>
 	<slot />
 </span>
