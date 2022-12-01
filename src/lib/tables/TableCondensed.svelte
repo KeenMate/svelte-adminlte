8dad8883e581cdc5afee47f1ff58434e1821aa8c
Filip Jakab <jakabfilip.fj@gmail.com>
2022-12-01 23:40:02 +0100

    Added option to add custom attributes on TableCondensed.svelte

--- src/lib/tables/TableCondensed.svelte
+++ src/lib/tables/TableCondensed.svelte
@@ -1,15 +1,12 @@
-<script>
-	import TableEmptyRow from "./TableEmptyRow.svelte"
-</script>
-
-<table class="table table-sm data-table {$$props.class || ''}">
+<table
+	{...$$restProps}
+	class="table table-sm data-table {$$props.class || ''}" 
+>
 	<thead>
 	<slot name="headers" />
 	</thead>
 	<tbody>
-	<slot>
-		<TableEmptyRow />
-	</slot>
+	<slot />
 	</tbody>
 	<slot name="footer" />
 </table>
