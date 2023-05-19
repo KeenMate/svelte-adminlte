d92a256ac433ce2154168ed0006f3f8b5ca3f801
Filip Jakab <jakabfilip.fj@gmail.com>
2023-05-19 22:21:55 +0200

    Added default values for ChatMessage.svelte to remove "undefined" from rendering to page

--- src/lib/ui/chat/ChatMessage.svelte
+++ src/lib/ui/chat/ChatMessage.svelte
@@ -1,23 +1,23 @@
 <script>
-	export let username
-	export let datetime
-	export let avatar
-	export let avatarAlt
-	export let message
+	export let username = null
+	export let datetime = null
+	export let avatar = null
+	export let avatarAlt = null
+	export let message = null
 	export let right = false
 </script>
 
 <div class="direct-chat-msg" class:right>
 	<div class="direct-chat-infos clearfix">
-		<span class="direct-chat-name float-left">{username}</span>
-		<span class="direct-chat-timestamp float-right">{datetime}</span>
+		<span class="direct-chat-name float-left">{username || ""}</span>
+		<span class="direct-chat-timestamp float-right">{datetime || ""}</span>
 	</div>
 	<slot name="avatar">
-		<img class="direct-chat-img" src={avatar} alt={avatarAlt}>
+		<img class="direct-chat-img" src={avatar || ""} alt={avatarAlt || ""}>
 	</slot>
 	<div class="direct-chat-text">
 		<slot>
-			{message}
+			{message || ""}
 		</slot>
 	</div>
 </div>
