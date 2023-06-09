9af54dfd6625540c48a3864649a9f4f5de0fa9fc
Filip Jakab <jakabfilip.fj@gmail.com>
2023-06-10 00:50:27 +0200

    Added few click events for ChatMessage.svelte

--- src/lib/ui/chat/ChatMessage.svelte
+++ src/lib/ui/chat/ChatMessage.svelte
@@ -1,4 +1,8 @@
 <script>
+	import {createEventDispatcher} from "svelte"
+
+	const dispatch = createEventDispatcher()
+	
 	export let username = null
 	export let datetime = null
 	export let avatar = null
@@ -7,17 +11,39 @@
 	export let right = false
 </script>
 
-<div class="direct-chat-msg" class:right>
+<div
+	class="direct-chat-msg"
+	class:right
+	on:click
+>
 	<div class="direct-chat-infos clearfix">
-		<span class="direct-chat-name float-left">{username || ""}</span>
-		<span class="direct-chat-timestamp float-right">{datetime || ""}</span>
+		<span
+			class="direct-chat-name float-left"
+			on:click={ev => dispatch("usernameClick", ev)}
+		>
+			{username || ""}
+		</span>
+		<span
+			class="direct-chat-timestamp float-right"
+			on:click={ev => dispatch("datetimeClick", ev)}
+		>
+			{datetime || ""}
+		</span>
 	</div>
 	<slot name="avatar">
 		{#if avatar || avatarAlt}
-			<img class="direct-chat-img" src={avatar || ""} alt={avatarAlt || ""}>
+			<img
+				class="direct-chat-img"
+				src={avatar || ""}
+				alt={avatarAlt || ""}
+	      on:click={ev => dispatch("avatarClick", ev)}
+			>
 		{/if}
 	</slot>
-	<div class="direct-chat-text">
+	<div
+		class="direct-chat-text"
+		on:click={ev => dispatch("chatBodyClicked", ev)}
+	>
 		<slot>
 			{message || ""}
 		</slot>
