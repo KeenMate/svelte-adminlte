4940a5ef0f40a310e16d32bf825ad29dfb005ffc
Ondrej Valenta <ondrej.valenta@keenmate.com>
2023-06-29 01:34:10 +0200

    SidebarNavItem.svelte now with target

---
+++ src/structure/navigation/SidebarNavItem.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let href = "#"
+	export let icon
+	export let active = false
+	export let tooltip = ""
+	export let target = ""
+</script>
+
+<li class="nav-item" data-toggle="tooltip" data-placement="right" title={tooltip}>
+	<a
+		class="nav-link"
+		class:active
+		{href}
+		{target}
+		{...$$restProps}
+	>
+		{#if icon}
+			<i class="nav-icon {icon}"></i>
+		{/if}
+
+		<slot />
+	</a>
+</li>
