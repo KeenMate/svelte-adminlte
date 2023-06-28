2a1add51b016043dbbcb38c4cf5719155a2170e1
Ondrej Valenta <ondrej.valenta@keenmate.com>
2023-06-29 01:53:35 +0200

    Missing `target` property because I'm an idiot

--- src/lib/structure/navigation/SidebarNavItem.svelte
+++ src/lib/structure/navigation/SidebarNavItem.svelte
@@ -3,10 +3,11 @@
 	export let icon
 	export let active = false
 	export let tooltip = ""
+	export let target = ""
 </script>
 
 <li class="nav-item" data-toggle="tooltip" data-placement="right" title={tooltip}>
-	<a class="nav-link" class:active {href}>
+	<a class="nav-link" class:active {href} {target}>
 		{#if icon}
 			<i class="nav-icon {icon}" />
 		{/if}
