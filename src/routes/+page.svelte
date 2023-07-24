94e987b54a832baed9adbde42cc1252dbeb0899f
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-07-24 20:10:16 +0200

    Install inputmask direcly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
 	import "icheck-bootstrap/icheck-bootstrap.css"
 	import "admin-lte/build/scss/AdminLTE.scss"
 	import "toastr/build/toastr.css"
+	import TextInput from "$lib/form/input/TextInput.svelte"
 </script>
 
 <div class="wrapper">Page</div>
+<TextInput />
