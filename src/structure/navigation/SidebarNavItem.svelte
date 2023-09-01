e8931b751499b90754910a435d06360bac6ced62
Filip Jakab <jakabfilip.fj@gmail.com>
2023-09-01 23:50:51 +0200

    Added more versatility for SidebarNavItem.svelte regarding icon

--- src/structure/navigation/SidebarNavItem.svelte
+++ src/structure/navigation/SidebarNavItem.svelte
@@ -1,17 +1,19 @@
-<script>
-	export let href = "#"
-	export let icon
-	export let active = false
-	export let tooltip = ""
-	export let target = ""
+<script lang="ts">
+	export let href: string | null = "#"
+	export let icon: string | null = null
+	export let active: boolean = false
+	export let tooltip: string | null = ""
+	export let target: string | null = ""
 </script>
 
 <li class="nav-item" data-toggle="tooltip" data-placement="right" title={tooltip}>
 	<a class="nav-link" class:active {href} {target} {...$$restProps}>
-		{#if icon}
-			<i class="nav-icon {icon}" />
-		{/if}
-
+		<slot name="icon">
+			{#if icon}
+				<i class="nav-icon {icon}" />
+			{/if}
+		</slot>
+		
 		<slot />
 	</a>
 </li>
