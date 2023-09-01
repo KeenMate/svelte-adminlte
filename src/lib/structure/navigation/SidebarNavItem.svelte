0b0d5e1d6d58d4400032f2615a7a1a694cb031f1
Filip Jakab <jakabfilip.fj@gmail.com>
2023-09-02 01:15:27 +0200

    Fixed issue with SidebarNavItem.svelte not being affected by changes :)

--- src/lib/structure/navigation/SidebarNavItem.svelte
+++ src/lib/structure/navigation/SidebarNavItem.svelte
@@ -1,15 +1,17 @@
-<script>
-	export let href = null
-	export let icon
-	export let active = false
-	export let tooltip = ""
-	export let target = ""
+<script lang="ts">
+	export let href: string | null = "#"
+	export let icon: string | null = null
+	export let active: boolean = false
+	export let tooltip: string | null = ""
+	export let target: string | null = ""
 </script>
 
 <li class="nav-item" data-toggle="tooltip" data-placement="right" title={tooltip}>
-	<a class="nav-link" class:active {href} {target}>
+	<a class="nav-link" class:active {href} {target} {...$$restProps}>
 		{#if icon}
 			<i class="nav-icon {icon}" />
+		{:else}
+			<slot name="icon" />
 		{/if}
 
 		<slot />
