30a69bc6c65f9a2ead4d89cb867f9ee3fae63cf2
Filip Jakab <jakabfilip.fj@gmail.com>
2023-09-12 02:58:54 +0200

    Removed unused debug log

--- src/lib/toasts/toastr.js
+++ src/lib/toasts/toastr.js
@@ -2,7 +2,6 @@ import toastr from "toastr"
 import {Config} from "$lib/config.js"
 
 Config.subscribe(config => {
-    console.log("Using toastr options from config")
     toastr.options = config.ToastrOptions
 })
 
