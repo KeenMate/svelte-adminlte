3bed19d329c6ec505e1a59a051d01c556579ead4
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-10-18 18:50:53 +0200

    Fix `TippyTooltip` to dont require `tippyOptions`

--- src/lib/ui/TippyTooltip.svelte
+++ src/lib/ui/TippyTooltip.svelte
@@ -1,31 +1,27 @@
 <script lang="ts">
-    import tippy, {type Instance} from "tippy.js"
-  import {onMount} from "svelte"
+	import tippy, {type Instance} from "tippy.js"
+	import {onMount} from "svelte"
 
-  export let content: string
-  export let tippyOptions: any
+	export let content: string
+	export let tippyOptions: any = null
 
-  let rootElement: HTMLElement
-  let tippyInstance: Instance
-    
-  $: tippyInstance?.setContent(content || "")
-  $: tippyOptions && tippyInstance?.setProps(tippyOptions)
+	let rootElement: HTMLElement
+	let tippyInstance: Instance
 
-  onMount(() => {
-    tippyInstance = tippy(rootElement)
-  })
+	$: tippyInstance?.setContent(content || "")
+	$: tippyOptions && tippyInstance?.setProps(tippyOptions)
+
+	onMount(() => {
+		tippyInstance = tippy(rootElement)
+	})
 </script>
 
-<div
-	bind:this={rootElement}
-	{...$$restProps}
-	class="tippy-parent {$$restProps.class || ''}"
->
-	<slot></slot>
+<div bind:this={rootElement} {...$$restProps} class="tippy-parent {$$restProps.class || ''}">
+	<slot />
 </div>
 
 <style lang="scss">
-  .tippy-parent {
-    display: inline-block;
-  }
+	.tippy-parent {
+		display: inline-block;
+	}
 </style>
