c6e9d11004a0f608ebf1b480e88e520bfa8da824
Filip Jakab <jakabfilip.fj@gmail.com>
2024-01-05 23:08:43 +0100

    Added new buttons

---
+++ src/lib/ui/buttons/AddButton.svelte
@@ -0,0 +1,22 @@
+<script>
+	import {LteButton} from "@keenmate/svelte-adminlte"
+	import {_} from "svelte-i18n"
+
+	export let xsmall = false
+	export let small = false
+	export let large = false
+
+	$: noSizeSet = !xsmall && !small && !large
+</script>
+
+<LteButton
+	color="success"
+	title={$_("common.buttons.add")}
+	xsmall={xsmall || noSizeSet}
+	{small}
+	{large}
+	on:click
+>
+	<i class="fas fa-plus fa-fw" />
+	<slot />
+</LteButton>
