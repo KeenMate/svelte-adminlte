5bfb5649398f94074eb3de11ec7ef038197f98f3
Filip Jakab <filip.jakab@keenmate.com>
2025-06-04 01:15:53 +0200

    feat: allowed ButtonDropdownItem to pass all props to its anchor element

--- src/lib/ui/dropdown/ButtonDropdownItem.svelte
+++ src/lib/ui/dropdown/ButtonDropdownItem.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	/**
+	 * @deprecated Use native `title` prop
+	 */
 	export let tooltip = ""
 </script>
 
-<a role="button" class="dropdown-item" title={tooltip} on:click>
+<a
+	role="button"
+	title={tooltip}
+	{...$$restProps}
+	class="dropdown-item {$$restProps.class || ''}"
+	on:click
+>
 	<slot />
 </a>
 
